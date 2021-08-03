df7865e870e2ee78673b3730edb8a20415a2e458
EmanueleFranceschini <ef96work@gmail>
2021-08-03 17:44:41 +0100

    design/functionality implementations

--- app.js
+++ app.js
@@ -1,27 +1,79 @@
-let mainContent = document.querySelector("#main-content");
-let btn = document.querySelector("#btn");
-let count = 0;
+let
+  mainCont = document.querySelector("#main-content")
+  btn = document.querySelector("#btn")
+  exists = false
+  advice = document.querySelector(".advice")
+  url = "https://api.adviceslip.com/advice"
+  btnIcon = document.querySelector(".fas.fa-lightbulb")
+  eListeners = ["click", "mouseenter", "mouseleave", "mousedown", "mouseup", "touchstart", "touchend"];
 
-btn.addEventListener("click", getAdvice);
 
-function getAdvice() {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.adviceslip.com/advice", true);
-  xhr.onload = function() {
-    if(this.status == 200) {
-      let response = JSON.parse(this.responseText);
-      console.log(this.responseText);
-      if (count == 0) {
-        count++;
-        let advice =  document.createElement("div");
-        advice.innerText = response.slip.advice;
-        advice.className = "advice";
-        mainContent.appendChild(advice);
-      }
-      else {
-        document.querySelector(".advice").innerText = response.slip.advice;
+eListeners.forEach(e => btn.addEventListener(e, eventsHandler));
+
+
+function eventsHandler() {
+
+  if (event.type == "click") {
+    // -- resetting BTN animation --
+    if (advice != undefined) {
+      advice.style.animation = "none";
+    }
+    // -- XHR call to fetch data from the API --
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = function() {
+      url += "?t=" + (new Date()).getTime();
+      if (this.status == 200) {
+        let response = JSON.parse(this.responseText);
+        if (exists === false) {
+          exists = true;
+          advice = document.createElement("p");
+          advice.innerHTML = `<i class="fas fa-quote-left"></i> ${response.slip.advice}`;
+          advice.className = "advice";
+          mainCont.appendChild(advice);
+          }
+        else {
+          document.querySelector(".advice").innerHTML = `<i class="fas fa-quote-left"></i> ${response.slip.advice}`;
+        }
+        advice.style.animation = "adviceAnimation ease-in 0.4s forwards";
       }
     }
+    xhr.send();
   }
-  xhr.send();
+
+  else if (event.type == "mouseenter") {
+    btn.style.transform = "scale(1.05)";
+    btn.style.transition = "transform 0.1s";
+    btnIcon.style.transform = "scale(1.05)";
+    btnIcon.style.transition = "transform 0.1s";
+
+  }
+
+  else if (event.type == "mouseleave") {
+    btn.style.transform = "scale(1)";
+    btnIcon.style.transform = "scale(1)";
+  }
+
+  else if (event.type == "mousedown") {
+    btn.style.borderColor = "var(--lightbulbCol)";
+  }
+
+  else if (event.type == "mouseup") {
+    btn.style.borderColor = "black";
+  }
+
+  else if (event.type == "touchstart") {
+    btn.style.transform = "scale(1.05)";
+    btn.style.transition = "transform 0.1s";
+    btn.style.borderColor = "var(--lightbulbCol)";
+    btnIcon.style.transform = "scale(1.05)";
+    btnIcon.style.transition = "transform 0.1s";
+  }
+
+  else if (event.type == "touchend") {
+    btn.style.transform = "scale(1)";
+    btn.style.borderColor = "black";
+    btnIcon.style.transform = "scale(1)";
+
+  }
 }
